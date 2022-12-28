16d1cd167eb50cd0033f0e4b7f290a339120e617
Yarden Naor <naoryarden@gmail.com>
2022-12-28 14:28:22 +0200

    build the note service

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,5 +1,75 @@
 
-export function noteService (){
+// import {storageService} from 
+
+import { storageService } from "../../../services/storage.service.js"
+import {asyncStorageService } from '../../../services/async-storage.service.js'
+
+const NOTE_KEY='noteDB'
+_makeNotes()
+
+export const noteService = {
+    query,
+    get,
+    remove,
+    save,
+    getEmptyNote
+}
+
+function query() {
+    return asyncStorageService.query(NOTE_KEY)
+        .then(notes)
+}
+
+function get(noteId) {
+    return asyncStorageService.get(NOTE_KEY, noteId)
+}
+
+function remove(noteId) {
+    return asyncStorageService.remove(NOTE_KEY, noteId)
+}
+
+function save(note) {
+    if (note.id) {
+        return asyncStorageService.put(NOTE_KEY, note)
+    } else {
+        return asyncStorageService.post(NOTE_KEY, note)
+    }
+}
+
+function  getEmptyNote(id='', type = '', isPinned = false, info={txt:''}) {
+    return { id, type, isPinned, info }
+}
+
+
+function _makeNotes (){
     console.log('Hi from note service')
+    let notes= storageService.loadFromStorage(NOTE_KEY)
+     if (!notes||!notes.length) notes=[
+        {
+         id: "n101",
+         type: "note-txt",
+         isPinned: true,
+         info: {
+         txt: "Fullstack Me Baby!"
+         }
+        },
+        {
+            id: "n101",
+            type: "note-txt",
+            isPinned: true,
+            info: {
+            txt: "hi hi!"
+            }
+           },
+           {
+            id: "n101",
+            type: "note-txt",
+            isPinned: true,
+            info: {
+            txt: "doing my sprint!"
+            }
+           }
+        ]
+        storageService.saveToStorage(NOTE_KEY,notes)
 }
 
--- apps/note/views/note-index.jsx
+++ apps/note/views/note-index.jsx
@@ -3,7 +3,7 @@ import { noteService } from "../services/note.service.js";
 
 
 export function NoteIndex() {
-
+    noteService.conect()
  console.log('hi from note index:')
 
 }
