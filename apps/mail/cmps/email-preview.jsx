3a08b7c8494d6cc401fa29bf22cc899c141f024a
Hadar5325 <hadar.r5325@gmail.com>
2022-12-29 19:09:09 +0200

    showin a read/unread state per email

--- apps/mail/cmps/email-preview.jsx
+++ apps/mail/cmps/email-preview.jsx
@@ -1,7 +1,10 @@
 
 export function MailPreview({ mail , onRemove}) {
-    return <article className="mail-preview">
+   console.log(mail)
+   const mailIsRead = (mail.isRead) ? 'true' : 'false'
+   return <article className="mail-preview">
         <div>
+            <div>mail is read: {mailIsRead}</div>
             <div>{mail.subject}</div>
             <div>{mail.sentAt}</div>
             <div>{mail.body}</div>
